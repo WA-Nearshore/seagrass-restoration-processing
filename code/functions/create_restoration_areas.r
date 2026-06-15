fec2a0c28740ac5f0f025292a7a8d7487d851f51
pdowty <peter.dowty@dnr.wa.gov>
2026-06-14 22:05:07 -0700

    debugging create_restoration_areas.

--- code/functions/create_restoration_areas.r
+++ code/functions/create_restoration_areas.r
@@ -12,7 +12,11 @@ library(tidyverse)
 library(sf)
 
 create_restoration_areas <- function(planting_locations, p_gps_pts1, pathFGDB) {
-  
+ 
+ # buffer dimension for point buffers that are grouped for convex hull areas
+ # dimensions follow EPSG 2927 so are in US Survey feet
+ buffer_dimension <- 1500
+   
  # get list of restoration areas from planting_locations
  restoration_area_names <- unique(planting_locations$restoration_area)
   
@@ -23,11 +27,14 @@ create_restoration_areas <- function(planting_locations, p_gps_pts1, pathFGDB) {
     st_as_sf(coords=c("longitude","latitude"), crs=4326) %>%
     st_transform(crs=2927)
  
- # read in 'baselayer' from FGDB with land polygons
- baselayer <- st_read(dsn = pathFGDB, layer = "baselayer")
+ # read in 'baselayer' from FGDB with land polygons; project to StPl Wash S
+ baselayer <- st_read(dsn = pathFGDB, layer = "baselayer") %>%
+   st_transform(crs=2927)
  
- # make convex hulls around grouped points  
- initial_hulls <- p_gps_pts_sf %>%
+ # make convex hulls around grouped point buffers 
+ buffered_pts <- st_buffer(p_gps_pts_sf, dist=buffer_dimension)
+ initial_hulls <- buffered_pts %>%
+   rename("restoration_area" = "site_location") %>%
    group_by(restoration_area) %>%
    summarize(geometry = st_combine(geometry)) %>%
    st_convex_hull()
@@ -35,10 +42,18 @@ create_restoration_areas <- function(planting_locations, p_gps_pts1, pathFGDB) {
  land_obstacles <- st_union(baselayer)
  
  final_hulls <- st_difference(initial_hulls, land_obstacles)
+
+ # temp debug 
+ st_write(p_gps_pts_sf, dsn=pathFGDB, layer="p_gps_pts_sf_test",
+           driver="OpenFileGDB", delete_layer=TRUE)
+ st_write(initial_hulls, dsn=pathFGDB, layer="initial_hulls",
+           driver="OpenFileGDB", delete_layer=TRUE)
+ 
  
  # write out to project file geodatabase
- st_write(final_nulls, dsn=pathFGDB, layer="restoration_areas",
+ st_write(initial_hulls, dsn=pathFGDB, layer="init_restoration_areas",
            driver="OpenFileGDB", delete_layer=TRUE)
+ # previsou write executed w/out error but not seen within fgdb from Pro.
   
 }
 
